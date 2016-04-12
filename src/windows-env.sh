298334c15fd90facc695bebc18c513ff717270ef
Dan Morriss <daniel_morriss@yahoo.com.au>
2016-04-12 20:34:37 +1000

    Added PATH update to make libsigc DLL findable on Windows.

--- src/windows-env.sh
+++ src/windows-env.sh
@@ -1,2 +1,3 @@
 export PKG_CONFIG_PATH=/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig
 #export PKG_CONFIG_PATH=/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig
+export PATH=$PATH:/usr/x86_64-w64-mingw32/sys-root/mingw/bin/
