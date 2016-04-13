ac5d9fecd3c700cfdc3c2138e67fccc3557d053e
Dan Morriss <dmorriss@aconex.com>
2016-04-13 16:14:43 +1000

    Scaling now partially working with +/- keys.

--- src/custom_widget.cc
+++ src/custom_widget.cc
@@ -10,23 +10,24 @@
 #define M_PI           3.14159265358979323846
 
 #include "custom_widget.h"
- 
-GType CustomWidget::gtype = 0;
 
-CustomWidget::CustomWidget (GtkDrawingArea *gobj) :
-  Gtk::DrawingArea (gobj)
+GType
+  CustomWidget::gtype = 0;
+
+CustomWidget::CustomWidget (GtkDrawingArea * gobj):
+Gtk::DrawingArea (gobj)
 {
 }
- 
-CustomWidget::CustomWidget () :
-  Glib::ObjectBase ("customwidget")  
+
+CustomWidget::CustomWidget ():
+Glib::ObjectBase ("customwidget")
 //: m_radius(0.42), m_line_width(0.05)
 {
 
   std::cerr << "IN constructor\n";
 //  add_events(Gdk::EXPOSURE_MASK);
-  add_events(Gdk::KEY_PRESS_MASK);
-  add_events(Gdk::BUTTON_PRESS_MASK);
+  add_events (Gdk::KEY_PRESS_MASK);
+  add_events (Gdk::BUTTON_PRESS_MASK);
 //  add_events(Gdk::SCROLL_MASK);
 //
   //signal_scroll_event().connect( sigc::mem_fun( *this, &CustomWidget::onScrollEvent ) );
@@ -34,10 +35,9 @@ CustomWidget::CustomWidget () :
 
 }
 
-Glib::ObjectBase *
-CustomWidget::wrap_new (GObject *o)
+Glib::ObjectBase * CustomWidget::wrap_new (GObject * o)
 {
-	std::cerr << "CustomWidget::wrap_now()\n";
+  std::cerr << "CustomWidget::wrap_now()\n";
 
   if (gtk_widget_is_toplevel (GTK_WIDGET (o)))
     {
@@ -45,14 +45,14 @@ CustomWidget::wrap_new (GObject *o)
     }
   else
     {
-      return Gtk::manage(new CustomWidget (GTK_DRAWING_AREA (o)));
+      return Gtk::manage (new CustomWidget (GTK_DRAWING_AREA (o)));
     }
 }
- 
+
 void
 CustomWidget::register_type ()
 {
-	std::cerr << "CustomWidget::register_type()\n";
+  std::cerr << "CustomWidget::register_type()\n";
   if (gtype)
     return;
 
@@ -62,181 +62,220 @@ CustomWidget::register_type ()
   Glib::wrap_register (gtype, CustomWidget::wrap_new);
 }
 
-bool CustomWidget::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
+bool
+CustomWidget::on_draw (const Cairo::RefPtr < Cairo::Context > &cr)
 {
 
-//	std::cerr << "CustomWidget::on_draw()\n";
+//      std::cerr << "CustomWidget::on_draw()\n";
 
-  Gtk::Allocation allocation = get_allocation();
-  const int width = allocation.get_width();
-  const int height = allocation.get_height();
+  Gtk::Allocation allocation = get_allocation ();
+  const int width = allocation.get_width ();
+  const int height = allocation.get_height ();
 
-  cr->scale(viewport_scale, viewport_scale);
-  cr->translate(0,0);
+  cr->scale (viewport_scale, viewport_scale);
+  cr->translate (0, 0);
   //cr->translate(0.1, 0.1);
   //cr->set_line_width(m_line_width);
-  cr->save();
+  cr->save ();
   //cr->set_source_rgba(0.337, 0.612, 0.117, 0.9);   
-  cr->set_source_rgba(0.2, 0.2, 0.2, 1.0);   
+  cr->set_source_rgba (0.2, 0.2, 0.2, 1.0);
 
-  cr->paint();
+  cr->paint ();
 
-  cr->set_source_rgba(0.8, 0.8, 0.8, 1.0);   
+  cr->set_source_rgba (0.8, 0.8, 0.8, 1.0);
 
-  cr->set_line_width(0.1);
-  cr->set_line_cap(Cairo::LINE_CAP_BUTT);
+  cr->set_line_width (0.1);
+  cr->set_line_cap (Cairo::LINE_CAP_BUTT);
 
   //cr->translate(-1920, -1080);
- 	//	cr->move_to(0,0);
-		cr->line_to(width, height);
-		cr->stroke();
+  //      cr->move_to(0,0);
+  cr->line_to (width, height);
+  cr->stroke ();
 
-  for (int x = 0 ; x < 1920 ; x +=8) {
-			if (!(x % 64)) {
-  				cr->set_line_width(0.2);
-  				cr->set_source_rgba(0.0, 0.0, 0.0, 1.0);   
-					} else {
-  				cr->set_line_width(0.1);
-  				cr->set_source_rgba(0.8, 0.8, 0.8, 1.0);   
-					} 
-	
-	    cr->move_to(x, 0);
-	    cr->line_to(x, 1080);
-    	cr->stroke();
- 			}
-
-  for (int y = 0 ; y < 1080 ; y +=8) {
-			if (!(y % 64)) {
-  				cr->set_line_width(0.2);
-  				cr->set_source_rgba(0.0, 0.0, 0.0, 1.0);   
-					} else {
-  				cr->set_line_width(0.1);
-  				cr->set_source_rgba(0.8, 0.8, 0.8, 1.0);   
-					} 
-	    cr->move_to(0, y);
-	    cr->line_to(1920, y);
-    	cr->stroke();
-			}
-
-  cr->restore();
-
-	return true;
-
-
-  cr->arc(0, 0, m_radius, 0, 2 * M_PI);
-  cr->save();
-  cr->set_source_rgba(1.0, 1.0, 1.0, 0.8);
-  cr->fill_preserve();
-  cr->restore();
-  cr->stroke_preserve();
-  cr->clip();
-
-  for (int i = 0; i < 12; i++)
-  {
-    double inset = 0.05;
-
-    cr->save();
-    cr->set_line_cap(Cairo::LINE_CAP_ROUND);
-
-    if(i % 3 != 0)
+  for (int x = 0; x < 1920; x += 8)
     {
-      inset *= 0.8;
-      cr->set_line_width(0.03);
+      if (!(x % 64))
+	{
+	  cr->set_line_width (0.2);
+	  cr->set_source_rgba (0.0, 0.0, 0.0, 1.0);
+	}
+      else
+	{
+	  cr->set_line_width (0.1);
+	  cr->set_source_rgba (0.8, 0.8, 0.8, 1.0);
+	}
+
+      cr->move_to (x, 0);
+      cr->line_to (x, 1080);
+      cr->stroke ();
     }
 
-    cr->move_to(
-      (m_radius - inset) * cos (i * M_PI / 6),
-      (m_radius - inset) * sin (i * M_PI / 6));
-    cr->line_to (
-      m_radius * cos (i * M_PI / 6),
-      m_radius * sin (i * M_PI / 6));
-    cr->stroke();
-    cr->restore(); 
-  }
+  for (int y = 0; y < 1080; y += 8)
+    {
+      if (!(y % 64))
+	{
+	  cr->set_line_width (0.2);
+	  cr->set_source_rgba (0.0, 0.0, 0.0, 1.0);
+	}
+      else
+	{
+	  cr->set_line_width (0.1);
+	  cr->set_source_rgba (0.8, 0.8, 0.8, 1.0);
+	}
+      cr->move_to (0, y);
+      cr->line_to (1920, y);
+      cr->stroke ();
+    }
+
+  cr->restore ();
+
+  return true;
+
+
+  cr->arc (0, 0, m_radius, 0, 2 * M_PI);
+  cr->save ();
+  cr->set_source_rgba (1.0, 1.0, 1.0, 0.8);
+  cr->fill_preserve ();
+  cr->restore ();
+  cr->stroke_preserve ();
+  cr->clip ();
+
+  for (int i = 0; i < 12; i++)
+    {
+      double inset = 0.05;
+
+      cr->save ();
+      cr->set_line_cap (Cairo::LINE_CAP_ROUND);
+
+      if (i % 3 != 0)
+	{
+	  inset *= 0.8;
+	  cr->set_line_width (0.03);
+	}
+
+      cr->move_to ((m_radius - inset) * cos (i * M_PI / 6),
+		   (m_radius - inset) * sin (i * M_PI / 6));
+      cr->line_to (m_radius * cos (i * M_PI / 6),
+		   m_radius * sin (i * M_PI / 6));
+      cr->stroke ();
+      cr->restore ();
+    }
 
   //std::cerr << "Getting the current time ...\n";
   // store the current time
   time_t rawtime;
-  time(&rawtime);
-  struct tm * timeinfo = localtime (&rawtime);
+  time (&rawtime);
+  struct tm *timeinfo = localtime (&rawtime);
 
   // compute the angles of the indicators of our clock
   double minutes = timeinfo->tm_min * M_PI / 30;
   double hours = timeinfo->tm_hour * M_PI / 6;
-  double seconds= timeinfo->tm_sec * M_PI / 30;
+  double seconds = timeinfo->tm_sec * M_PI / 30;
 
-  cr->save();
-  cr->set_line_cap(Cairo::LINE_CAP_ROUND);
+  cr->save ();
+  cr->set_line_cap (Cairo::LINE_CAP_ROUND);
 
   // draw the seconds hand
-  cr->save();
-  cr->set_line_width(m_line_width / 3);
-  cr->set_source_rgba(0.7, 0.7, 0.7, 0.8); // gray
-  cr->move_to(0, 0);
-  cr->line_to(sin(seconds) * (m_radius * 0.9),
-    -cos(seconds) * (m_radius * 0.9));
-  cr->stroke();
-  cr->restore();
+  cr->save ();
+  cr->set_line_width (m_line_width / 3);
+  cr->set_source_rgba (0.7, 0.7, 0.7, 0.8);	// gray
+  cr->move_to (0, 0);
+  cr->line_to (sin (seconds) * (m_radius * 0.9),
+	       -cos (seconds) * (m_radius * 0.9));
+  cr->stroke ();
+  cr->restore ();
 
   // draw the minutes hand
-  cr->set_source_rgba(0.117, 0.337, 0.612, 0.9);   // blue
-  cr->move_to(0, 0);
-  cr->line_to(sin(minutes + seconds / 60) * (m_radius * 0.8),
-    -cos(minutes + seconds / 60) * (m_radius * 0.8));
-  cr->stroke();
+  cr->set_source_rgba (0.117, 0.337, 0.612, 0.9);	// blue
+  cr->move_to (0, 0);
+  cr->line_to (sin (minutes + seconds / 60) * (m_radius * 0.8),
+	       -cos (minutes + seconds / 60) * (m_radius * 0.8));
+  cr->stroke ();
 
   // draw the hours hand
-  cr->set_source_rgba(0.337, 0.612, 0.117, 0.9);   // green
-  cr->move_to(0, 0);
-  cr->line_to(sin(hours + minutes / 12.0) * (m_radius * 0.5),
-    -cos(hours + minutes / 12.0) * (m_radius * 0.5));
-  cr->stroke();
-  cr->restore();
+  cr->set_source_rgba (0.337, 0.612, 0.117, 0.9);	// green
+  cr->move_to (0, 0);
+  cr->line_to (sin (hours + minutes / 12.0) * (m_radius * 0.5),
+	       -cos (hours + minutes / 12.0) * (m_radius * 0.5));
+  cr->stroke ();
+  cr->restore ();
 
   // draw a little dot in the middle
-  cr->arc(0, 0, m_line_width / 3.0, 0, 2 * M_PI);
-  cr->fill();
+  cr->arc (0, 0, m_line_width / 3.0, 0, 2 * M_PI);
+  cr->fill ();
 
-	return true;
+  return true;
 
 }
 
-bool CustomWidget::on_timeout()
+bool
+CustomWidget::on_timeout ()
 {
 
-	  //std::cerr << "CustomWidget::on_timeout()\n";
-    // force our program to redraw the entire clock.
-    auto win = get_window();
-    if (win)
+  //std::cerr << "CustomWidget::on_timeout()\n";
+  // force our program to redraw the entire clock.
+  auto win = get_window ();
+  if (win)
     {
-        Gdk::Rectangle r(0, 0, get_allocation().get_width(),
-                get_allocation().get_height());
-        win->invalidate_rect(r, false);
+      Gdk::Rectangle r (0, 0, get_allocation ().get_width (),
+			get_allocation ().get_height ());
+      win->invalidate_rect (r, false);
     }
-    return true;
+  return true;
 }
 
-void CustomWidget::enable_timeout()
+void
+CustomWidget::enable_timeout ()
 {
-	Glib::signal_timeout().connect( sigc::mem_fun(*this, &CustomWidget::on_timeout), 1000 );
-  add_events(Gdk::BUTTON_PRESS_MASK);
- // add_events(Gdk::KEY_PRESS_MASK);
+  Glib::signal_timeout ().
+    connect (sigc::mem_fun (*this, &CustomWidget::on_timeout), 1000);
+  add_events (Gdk::BUTTON_PRESS_MASK);
+  // add_events(Gdk::KEY_PRESS_MASK);
   //set_flags(Gtk::CAN_FOCUS);
-   grab_focus(); 
- // signal_key_press_event().connect( sigc::ptr_fun(&CustomWidget::on_key_press_event); 
+  grab_focus ();
+  // signal_key_press_event().connect( sigc::ptr_fun(&CustomWidget::on_key_press_event); 
 }
 
 
-bool CustomWidget::on_button_press_event(GdkEventButton *event)
+bool
+CustomWidget::on_button_press_event (GdkEventButton * event)
 {
 
-	std::cerr << "MOUSE BUTTON!!\n";
-	return true;
+  std::cerr << "MOUSE BUTTON!!\n";
+  return true;
 }
 
-bool CustomWidget::on_key_press_event(GdkEventKey* event)
+bool
+CustomWidget::on_key_press_event (GdkEventKey * event)
 {
-	std::cerr << "KEYBOARD BUTTON!!\n";
-	return true;
+  std::cerr << "KEYBOARD BUTTON EVENT TYPE: " << event->type << "\n";
+  unsigned char inputdata;
+
+  if (event->type == GDK_KEY_PRESS)
+    {
+      std::cerr << "Input was: " << event->string << "\n";
+      inputdata = 0;
+      if (event->string)
+	{
+	  inputdata = event->string[0];
+	  switch (inputdata)
+	    {
+	    case '+':
+				if (viewport_scale <= 3) viewport_scale++;
+	      viewport_scale++;
+	      break;
+	    case '-':
+				if  (viewport_scale >=1) viewport_scale--;
+	      break;
+
+	    default:
+	      break;
+	    }
+	}
+
+    }
+
+  show_now ();
+  return true;
 
 }
