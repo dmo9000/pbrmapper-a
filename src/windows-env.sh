aecc9ab19a017b33e1e4ce397220f9116fdd2abb
Daniel Morriss <daniel_morriss@yahoo.com.au>
2016-04-14 15:06:06 +1000

    Improve PATH and PKG_CONFIG_PATH selection depending on either 32-bit or 64-bit windows.

--- src/windows-env.sh
+++ src/windows-env.sh
@@ -1,3 +1,15 @@
-export PKG_CONFIG_PATH=/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig
-#export PKG_CONFIG_PATH=/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig
-export PATH=$PATH:/usr/x86_64-w64-mingw32/sys-root/mingw/bin/
+CONFIG_GUESS=`./config.guess`
+
+case $CONFIG_GUESS in
+  i686-pc-cygwin)
+  export PKG_CONFIG_PATH=/usr/i686-w64-mingw32/sys-root/mingw/lib/pkgconfig
+  export PATH=$PATH:/usr/i686-w64-mingw32/sys-root/mingw/bin/
+  echo "32-bit mingw32 build environment configured"
+  ;;
+  x86_64-pc-cygwin)
+  export PKG_CONFIG_PATH=/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig
+  export PATH=$PATH:/usr/x86_64-w64-mingw32/sys-root/mingw/bin/
+  echo "64-bit mingw32 build environment configured"
+  ;;
+esac
+
