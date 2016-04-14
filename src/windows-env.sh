d62a7f3d2a0166431b609fa63cd0ba9506eff789
Dan Morriss <dmorriss@aconex.com>
2016-04-14 15:10:54 +1000

    Print error when trying to run on non-Cygwin.

--- src/windows-env.sh
+++ src/windows-env.sh
@@ -10,6 +10,9 @@ case $CONFIG_GUESS in
   export PKG_CONFIG_PATH=/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig
   export PATH=$PATH:/usr/x86_64-w64-mingw32/sys-root/mingw/bin/
   echo "64-bit mingw32 build environment configured"
+	;;
+	*)
+	echo "Sorry, this script is only required for Microsoft Windows running a Cygwin environment."
   ;;
 esac
 
