258155375ab73db21face072b7ce1ea377cc4bb3
Dan Morriss <daniel_morriss@yahoo.com.au>
2016-04-21 23:06:51 +1000

    Finish writing output labels and fix broken indenting from previous commit.

--- src/simple.cc
+++ src/simple.cc
@@ -251,6 +251,16 @@ void on_saveas_clicked()
                 fprintf (stderr, "failure writing input\n");
                 return;
             }
+
+            std::string label = nodeptr->GetPortLabel(i, SOCKTYPE_OUTPUT);
+            memset(&buffer, 0, 256);
+            snprintf((char *) &buffer, 255, "%s", label.c_str());
+            rc = xmlTextWriterWriteAttribute(writer, BAD_CAST "label", BAD_CAST buffer);
+            if (rc < 0) {
+                fprintf (stderr, "failure writing output label\n");
+                return;
+            }
+
             rc = xmlTextWriterEndElement(writer);
             if (rc < 0) {
                 printf("testXmlwriterDoc: Error at xmlTextWriterEndElement\n");
@@ -282,66 +292,67 @@ void on_saveas_clicked()
 
     xmlFreeTextWriter(writer);
     return;
+}
 
-    int main (int argc, char **argv)
+int main (int argc, char **argv)
+{
+    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+    custom_widgets_register();
+
+    //Load the GtkBuilder file and instantiate its widgets:
+    auto refBuilder = Gtk::Builder::create();
+    try
     {
-        auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
-        custom_widgets_register();
-
-        //Load the GtkBuilder file and instantiate its widgets:
-        auto refBuilder = Gtk::Builder::create();
-        try
-        {
-            refBuilder->add_from_file("glade/default_layout.glade");
-        }
-        catch(const Glib::FileError& ex)
-        {
-            std::cerr << "FileError: " << ex.what() << std::endl;
-            return 1;
-        }
-        catch(const Glib::MarkupError& ex)
-        {
-            std::cerr << "MarkupError: " << ex.what() << std::endl;
-            return 1;
-        }
-        catch(const Gtk::BuilderError& ex)
-        {
-            std::cerr << "BuilderError: " << ex.what() << std::endl;
-            return 1;
-        }
-
-        //Get the GtkBuilder-instantiated Dialog:
-        refBuilder->get_widget("applicationwindow1", pMainWindow);
-        pMainWindow->set_title("Substance Instainer");
-        refBuilder->get_widget("statusbar1", pStatusBar);
-        pStatusBar->push("Welcome to Substance Instainer!");
-        refBuilder->get_widget("viewport1", pViewPort);
-
-        refBuilder->get_widget("customwidget1", pCustomWidget);
-        pCustomWidget->show_now();
-        pCustomWidget->enable_timeout();
-
-        /* initialise GEGL */
-
-        geglio_init();
-
-        if(pMainWindow)
-        {
-
-            /* Connect File menu callbacks */
-            Gtk::ImageMenuItem* pButton = nullptr;
-            refBuilder->get_widget("imagemenuitem3", pButton);
-            if(pButton) pButton->signal_activate().connect( sigc::ptr_fun(on_saveas_clicked));
-            refBuilder->get_widget("imagemenuitem4", pButton);
-            if(pButton) pButton->signal_activate().connect( sigc::ptr_fun(on_saveas_clicked));
-            refBuilder->get_widget("imagemenuitem5", pButton);
-            if(pButton) pButton->signal_activate().connect( sigc::ptr_fun(on_button_clicked));
-
-            app->run(*pMainWindow);
-        }
-
-        delete pMainWindow;
-        geglio_exit();
-
-        return 0;
+        refBuilder->add_from_file("glade/default_layout.glade");
     }
+    catch(const Glib::FileError& ex)
+    {
+        std::cerr << "FileError: " << ex.what() << std::endl;
+        return 1;
+    }
+    catch(const Glib::MarkupError& ex)
+    {
+        std::cerr << "MarkupError: " << ex.what() << std::endl;
+        return 1;
+    }
+    catch(const Gtk::BuilderError& ex)
+    {
+        std::cerr << "BuilderError: " << ex.what() << std::endl;
+        return 1;
+    }
+
+    //Get the GtkBuilder-instantiated Dialog:
+    refBuilder->get_widget("applicationwindow1", pMainWindow);
+    pMainWindow->set_title("Substance Instainer");
+    refBuilder->get_widget("statusbar1", pStatusBar);
+    pStatusBar->push("Welcome to Substance Instainer!");
+    refBuilder->get_widget("viewport1", pViewPort);
+
+    refBuilder->get_widget("customwidget1", pCustomWidget);
+    pCustomWidget->show_now();
+    pCustomWidget->enable_timeout();
+
+    /* initialise GEGL */
+
+    geglio_init();
+
+    if(pMainWindow)
+    {
+
+        /* Connect File menu callbacks */
+        Gtk::ImageMenuItem* pButton = nullptr;
+        refBuilder->get_widget("imagemenuitem3", pButton);
+        if(pButton) pButton->signal_activate().connect( sigc::ptr_fun(on_saveas_clicked));
+        refBuilder->get_widget("imagemenuitem4", pButton);
+        if(pButton) pButton->signal_activate().connect( sigc::ptr_fun(on_saveas_clicked));
+        refBuilder->get_widget("imagemenuitem5", pButton);
+        if(pButton) pButton->signal_activate().connect( sigc::ptr_fun(on_button_clicked));
+
+        app->run(*pMainWindow);
+    }
+
+    delete pMainWindow;
+    geglio_exit();
+
+    return 0;
+}
